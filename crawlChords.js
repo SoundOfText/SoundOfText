9c1188add137ac102d47a56074390138391a53fa
Allie <alahnala@gmail.com>
2018-09-26 10:38:24 -0400

    changed song

--- crawlChords.js
+++ crawlChords.js
@@ -1,6 +1,6 @@
 const ugs = require('ultimate-guitar-scraper')
  
-let tabUrl = 'https://tabs.ultimate-guitar.com/n/nirvana/smells_like_teen_spirit_ver2_crd.htm'
+let tabUrl = 'https://tabs.ultimate-guitar.com/tab/frank_sinatra/fly_me_to_the_moon_chords_335196'
 ugs.get(tabUrl, (error, tab) => {
   if (error) {
     console.log(error)
