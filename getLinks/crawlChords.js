b834b1e4f66440199fb3286f7ad79d2fa3ef5cb5
Allie <alahnala@gmail.com>
2018-09-27 22:25:31 -0400

    it all works now yay

--- getLinks/crawlChords.js
+++ getLinks/crawlChords.js
@@ -5,33 +5,46 @@ const Lyrics = "body > div.js-page.js-global-wrapper > div > div._16KsA > main >
 
 
 async function getLinks() {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   var total_links = 0;
   var list;
+  var chordsPage = 'https://www.ultimate-guitar.com/explore?page=1&type[]=Chords'
+  var pageNumber = 1;
+  console.log(pageNumber.toString());
+  var testChordPage = chordsPage.replace(/page=.*&type/, 'page='+pageNumber.toString()+'&type');
+  console.log(testChordPage);
   
-  await page.goto('https://www.ultimate-guitar.com/explore?order=artistname_asc&type[]=Chords');
+  await page.goto(chordsPage);
   await page.setViewport({width: 1000, height: 500})
   await page.screenshot({ path: 'screenshots/ultimateguitar.png' });
 
-  while(await page.$(NEXT_PAGE) != null){
-    //hi matt, this #Rectangle here is the css selector for the X button on the add
-    if(await page.$("#Rectangle") != null){
-      page.click("#Rectangle");
-    // await page.waitFor(() => !document.querySelector("#Rectangle"));
+  while(true){
+    try{
+      //hi matt, this #Rectangle here is the css selector for the X button on the add
+      if(await page.$("#Rectangle") != null){
+        page.click("#Rectangle");
+      // await page.waitFor(() => !document.querySelector("#Rectangle"));
+      }
+      await page.setViewport({width: 2000, height: 1000})
+
+      list = await page.evaluateHandle(() => {
+        return Array.from(document.getElementsByClassName('link-primary _1kcZ5')).map(a => a.href);
+      });
+      console.log(await list.jsonValue());
+      page.click(NEXT_PAGE);
+      await page.waitForNavigation();
+      await page.screenshot({ path: 'screenshots/nextpage.png' });
     }
-    await page.setViewport({width: 2000, height: 1000})
-
-    console.log("out of the if statement, ad should be gone");
-
-    list = await page.evaluateHandle(() => {
-      return Array.from(document.getElementsByClassName('link-primary _1kcZ5')).map(a => a.href);
-    });
-    console.log(await list.jsonValue());
-    page.click(NEXT_PAGE);
-    await page.waitForNavigation();
-    await page.screenshot({ path: 'screenshots/nextpage.png' });
-
+    catch{
+      console.log("something didn't work at", chordsPage);
+      
+    }
+    pageNumber++;
+    pageNumber.toString();
+    console.log(pageNumber.toString());
+    chordsPage = chordsPage.replace(/page=.*&type/, 'page='+pageNumber.toString()+'&type');
+    await page.goto(chordsPage);
   }
   browser.close();
 
